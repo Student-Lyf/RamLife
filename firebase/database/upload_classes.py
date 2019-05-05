6cf948a4851af039a211b7b6c2f89ce047a7b496
Levi Lesches <levilesches@gmail.com>
2019-05-04 22:23:41 -0400

    Started Python script

--- firebase/database/upload_classes.py
+++ firebase/database/upload_classes.py
@@ -1,10 +1,10 @@
 from db import get_db
-from data.classes import Class
+from data.subjects import Subject
 
 db = get_db().collection("classes")
 
-def upload_class(class_: Class): 
-	doc = db.document(class_.id)
-	doc.set(class_.output())
+def upload_class(subject: Subject): 
+	doc = db.document(subject.id)
+	doc.set(subject.output())
 
-upload_class(Class.random())
+# upload_class(Class.random())
