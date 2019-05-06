3319abea62672d186989784a3cd3d3fef8cc6c42
Levi Lesches <levilesches@gmail.com>
2019-05-06 13:40:37 -0400

    Finished sports page

---
+++ lib/pages/sports.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+import "package:ramaz/data/sports.dart" show SportsGame;
+import "package:ramaz/widgets/sports_tile.dart";
+
+class SportsPage extends StatelessWidget {
+	static const List<Tab> tabs = [
+		Tab(text: "Recent"),
+		Tab(text: "Upcoming")
+	];
+
+	final List<SportsGame> recent, upcoming;
+	SportsPage._ (this.recent, this.upcoming);
+
+	factory SportsPage(List<SportsGame> games) {
+		final DateTime now = DateTime.now();
+		final List<SportsGame> recent = games.where(
+			(SportsGame game) => game.time < now
+		).toList();
+		final List<SportsGame> upcoming = games.where (
+			(SportsGame game) => game.time > now
+		).toList();
+		return SportsPage._(recent, upcoming);
+	}
+
+	@override Widget build(BuildContext context) => DefaultTabController (
+		length: 2,
+		child: Scaffold (
+			appBar: AppBar (
+				title: Text ("Sports"),
+				bottom: TabBar (tabs: tabs)
+			),
+			body: TabBarView (
+				children: tabs.map (
+					(Tab tab) => ListView (
+						children: [recent, upcoming].map(
+							(List<SportsGame> gamesList) => ListView (
+								children: gamesList.map (
+									(SportsGame game) => SportsTile (game)
+								).toList()
+							)
+						).toList()
+					)
+				).toList()
+			)
+		)
+	);
+}
