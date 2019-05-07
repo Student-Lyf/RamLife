bd3869aee98afc71840863f10a38ae6750a71ead
Levi Lesches <levilesches@gmail.com>
2019-05-07 18:13:09 -0400

    Massive re-organization

--- firebase/database/data/time_.py
+++
@@ -1,9 +0,0 @@
-from my_stuff.misc import init
-
-class Time: 
-	@init
-	def __init__(self, hour: int, minutes: int): pass
-
-class Range: 
-	@init
-	def __init__(self, start: Time, end: Time): pass
---
+++ firebase/database/students.py
@@ -0,0 +1,11 @@
+from firebase_admin import firestore
+
+db = firestore.client()
+STUDENTS = "students"
+
+def upload_students (students): 
+	batch = db.batch()
+	for student in students: 
+		doc = db.collection (STUDENTS).document (student.username)
+		batch.set (doc, student.output())
+	batch.commit()
