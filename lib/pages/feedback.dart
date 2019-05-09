d6705147ea0b2a9c0b38e472caf1134620d7da7e
Levi Lesches <levilesches@gmail.com>
2019-05-09 15:23:59 -0400

    Designed textbox

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -1,10 +1,39 @@
 import "package:flutter/material.dart";
 
+import "package:ramaz/pages/drawer.dart" show NavigationDrawer;
+
 class FeedbackPage extends StatelessWidget {
+	final TextEditingController controller = TextEditingController();
+
 	@override Widget build (BuildContext context) => Scaffold (
 		appBar: AppBar (title: Text ("Send Feedback")),
-		body: Column (
-			children: []
+		drawer: NavigationDrawer(),
+		body: Padding (
+			padding: EdgeInsets.symmetric (horizontal: 50),
+			child: Column (
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: [
+					// SizedBox (height: 50),
+					Center (
+						child: TextField (
+							autofocus: true,
+							controller: controller,
+							maxLength: 500,
+						)
+					),
+					SizedBox(height: 50),
+					RaisedButton.icon(
+						label: Text ("Submit"),
+						icon: Icon (Icons.send),
+						onPressed: submit,
+					)
+				]
+			)
 		)
 	);
+
+	void submit() {
+		final String feedback = controller.text;
+		print (feedback);
+	}
 }
