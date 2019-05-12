8b60796fec91a0ba67985ae0157df85e9f2dfc7f
Levi Lesches <levilesches@gmail.com>
2019-05-12 17:12:07 -0400

    Separation of day and lunch

--- lib/mock/day.dart
+++ lib/mock/day.dart
@@ -1,14 +1,16 @@
 import "package:ramaz/data/schedule.dart";
 
-Day placeholderToday = Day (
+const Lunch lunch = Lunch(
+	main: "Fish Tacos",
+	soup: "Navy Bean soup",
+	side1: "Roasted Broccoli",
+	side2: "Sweet Potato Wedges",
+	salad: "Greek Salad",
+);
+
+final Day placeholderToday = Day (
 	letter: Letters.C,
-	lunch: Lunch (  // Lunch for 2/7/19
-		soup: "Navy Bean soup",
-		main: "Fish Tacos",
-		side1: "Roasted Broccoli",
-		side2: "Sweet Potato Wedges",
-		salad: "Greek Salad",
-	)
+	lunch: null
 );
 
 // ----------------Mocked functions----------------
