c0fee68e1544503be28b3360c5ca90825cd7e9b4
Levi Lesches <levilesches@gmail.com>
2019-05-12 13:34:00 -0400

    Added a Day class to the Python script

--- data/calendar/calendar.py
+++ data/calendar/calendar.py
@@ -2,12 +2,20 @@ from pathlib import Path
 from csv import DictReader as Reader
 from datetime import datetime
 
-def parse_entry(entry): 
+from my_stuff.misc import init
+
+class Day: 
+	@init
+	def __init__(self, date, letter): pass
+	def __repr__(self): 
+		if (self.letter): return f"{self.date}: {self.letter}"
+		else: return f"{self.date}: No School"
+
+def parse_entry(entry) -> Day: 
 	month, day, year = entry ["date"].split("/")
-	# print (entry["date"].split("/"))
 	date = datetime(int (year), int (month), int (day))
 	letter = entry["letter"]
-	return date, letter
+	return Day (date, letter)
 
 def parse_calendar(path): 
 	path = Path (path)
@@ -31,4 +39,4 @@ def parse_calendar(path):
 		]
 
 entries = parse_calendar(r"C:\users\levi\coding\flutter\ramaz\data\calendar\calendar.csv")
-print (len (entries))
+print (entries)
