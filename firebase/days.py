8b3a299e17ebba1a5a5acdb3a196311ccc12bb43
Levi Lesches <levilesches@gmail.com>
2019-05-12 21:09:14 -0400

    re-organized the firebase data folder

--- firebase/days.py
+++ firebase/days.py
@@ -5,6 +5,6 @@ from database.calendar import parse_calendar, upload_calendar
 
 upload_calendar(
 	parse_calendar(
-		r"C:\users\levi\coding\flutter\ramaz\data\calendar\calendar.csv"
+		r"C:\users\levi\coding\flutter\ramaz\data\calendar.csv"
 	)
 )
