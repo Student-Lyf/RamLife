3df438424e90bb61e0d69f4c131e56de360fc6f7
Levi Lesches <levilesches@gmail.com>
2019-05-16 22:23:45 -0400

    redid pages/login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -24,8 +24,7 @@ class LoginState extends State <Login> {
 	static final RegExp usernameRegex = RegExp ("[a-z]+");
 	static final RegExp passwordRegex = RegExp (r"([a-z]|\d)+");
 	final TextEditingController usernameController = TextEditingController();
-	final TextEditingController passwordController = TextEditingController();
-	final FocusNode userNode = FocusNode()..addListener(verifyUser);
+	final FocusNode userNode = FocusNode();
 	final GlobalKey<ScaffoldState> key = GlobalKey();
 	String usernameError, passwordError;
 
@@ -33,12 +32,12 @@ class LoginState extends State <Login> {
 		super.initState();
 		Auth.signOut();  // To log in, one must first log out  --Levi
 		widget.reader.deleteAll();
+		userNode.addListener(verifyUser);
 	}
 
 	@override void dispose() {
 		super.dispose();
 		usernameController.dispose();
-		passwordController.dispose();
 	}
 
 	@override
@@ -52,13 +51,18 @@ class LoginState extends State <Login> {
 					children: [
 						RamazLogos.teal,
 						DefaultTabController(
-							length: 3,
+							length: 2,
 							child: TabBarView (
 								physics: NeverScrollableScrollPhysics(),
 								children: [
 									Column (
 										children: [
 											TextField(
+												controller: usernameController,
+												focusNode: userNode,
+												textInputAction: TextInputAction.next,
+												onChanged: validateUsername,
+												onSubmitted: verifyUser,
 												decoration: InputDecoration(
 													icon: Icon (Icons.verified_user),
 													labelText: "Username",
@@ -77,6 +81,7 @@ class LoginState extends State <Login> {
 											)
 										]
 									),
+									CircularProgressIndicator(),
 								]
 							)
 						)
@@ -89,46 +94,49 @@ class LoginState extends State <Login> {
 	static bool capturesAll (String text, RegExp regex) => 
 		text.isEmpty || regex.matchAsPrefix(text)?.end == text.length;
 
-	void passwordValidator (String pass) => setState(() {
-		passwordError = capturesAll (pass, passwordRegex)
-			? null
-			: "Only lower case letters and numbers allowed";
-		ready = pass.isNotEmpty && usernameController.text.isNotEmpty;
-	});
+	// void passwordValidator (String pass) => setState(() {
+	// 	passwordError = capturesAll (pass, passwordRegex)
+	// 		? null
+	// 		: "Only lower case letters and numbers allowed";
+	// 	ready = pass.isNotEmpty && usernameController.text.isNotEmpty;
+	// });
 
-	void usernameValidate(String text) => setState(() {
+	void validateUsername(String text) {
+		String error;
 		if (text.contains("@"))
-			usernameError = "Do not enter your Ramaz email, just your username";
-		else if (capturesAll (text, usernameRegex)) usernameError = null;
-		else usernameError = "Only lower case letters allowed";
-		ready = text.isNotEmpty && passwordController.text.isNotEmpty; 
-	});
+			error = "Do not enter your Ramaz email, just your username";
+		else if (!capturesAll (text, usernameRegex))
+			error = "Only lower case letters allowed";
+		if (error != null) setState(() => usernameError = error);
+	}
 
-	void transition ([String username]) => FocusScope.of(context)
-		.requestFocus(_passwordNode);
+	void verifyUser([String username]) {
+		// TODO: Check if user exists and has email set up
+	}
+
+	void transition ([String username]) => DefaultTabController.of(context).animateTo(1);
 
 	void login () async {
-		final String username = usernameController.text;
-		final String password = passwordController.text;
-		try {await Auth.signIn(username, password);}
-		on PlatformException catch (error) {
-			switch (error.code) {
-				case "ERROR_USER_NOT_FOUND":
-					setState(() => usernameError = "User does not exist");
-					break;
-				case "ERROR_WRONG_PASSWORD": 
-					// Check if we can sign in with a password
-					if ((await Auth.getSignInMethods(username)).contains ("password")) 
-						setState(() => passwordError = "Invalid password");
-					else setState(
-						() => passwordError = "This account has no password -- sign in with Google."
-					);
-					break;
-				default: throw "Cannot handle error: ${error.code}";
-			}
-			return;
-		}
-		downloadData(username);
+		// final String username = usernameController.text;
+		// try {await Auth.signIn(username, password);}
+		// on PlatformException catch (error) {
+		// 	switch (error.code) {
+		// 		case "ERROR_USER_NOT_FOUND":
+		// 			setState(() => usernameError = "User does not exist");
+		// 			break;
+		// 		case "ERROR_WRONG_PASSWORD": 
+		// 			// Check if we can sign in with a password
+		// 			if ((await Auth.getSignInMethods(username)).contains ("password")) 
+		// 				setState(() => passwordError = "Invalid password");
+		// 			else setState(
+		// 				() => passwordError = "This account has no password -- sign in with Google."
+		// 			);
+		// 			break;
+		// 		default: throw "Cannot handle error: ${error.code}";
+		// 	}
+		// 	return;
+		// }
+		// downloadData(username);
 	}
 
 	void googleLogin() async {
@@ -149,10 +157,6 @@ class LoginState extends State <Login> {
 				content: Text ("Make sure to use Google to sign in next time")
 			)
 		); 
-		setState(() {
-			loading = true;
-			ready = true;
-		});
 		final Map<String, dynamic> data = (await Firestore.getStudent(username)).data;
 		widget.reader.studentData = data;
 		widget.reader.student = Student.fromData(data);
