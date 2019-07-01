592b1e4743a58a7fc2e147aa11b9f4e1f06f8e62
Levi Lesches <levilesches@gmail.com>
2019-06-30 20:10:07 -0400

    Added course ID to the drop-down list
    NOTE: ID is **specifically** not added to `Period.info` because it should not show up on the home screen

--- lib/widgets/class_list.dart
+++ lib/widgets/class_list.dart
@@ -4,22 +4,31 @@ import "package:ramaz/data/schedule.dart" show Period, Subject;
 import "package:ramaz/services/reader.dart";
 
 class ClassPanel extends StatelessWidget {
-	final String title;
+	final String title, id;
 	final List <Widget> children;
 
 	const ClassPanel ({
 		@required this.title,
 		@required this.children,
+		@required this.id,
 	});
 
 	@override Widget build (BuildContext context) => ExpansionTile (
-		title: Text(title),
+		// title: ListTile (
+		// 	title: Text (title),
+		// 	subtitle: Text (id ?? ""),
+		// 	contentPadding: EdgeInsets.symmetric(vertical: 0),
+		// ),
+		title: Text (title),
 		children: [
 			Align (
 				alignment: const Alignment (-0.75, 0),
 				child: Column (
 					crossAxisAlignment: CrossAxisAlignment.start,
-					children: children.map (
+					children: [
+						...children, 
+						if (id != null) Text ("ID: $id")
+					].map (
 						(Widget child) => Padding (
 							padding: const EdgeInsets.symmetric(vertical: 5),
 							child: child
@@ -36,7 +45,7 @@ class ClassList extends StatelessWidget {
 	final Iterable<Period> periods;
 	final String headerText;
 	final Reader reader;
-	const ClassList ({
+	ClassList ({
 		@required this.periods, 
 		@required this.reader,
 		this.headerText
@@ -44,9 +53,8 @@ class ClassList extends StatelessWidget {
 
 	@override Widget build (BuildContext context) => ListView (
 		shrinkWrap: true,
-		children: (headerText == null 
-			? const <Widget> [] 
-			: <Widget>[
+		children: [
+			if (headerText != null) 
 				DrawerHeader (
 					child: Center (
 						child: Text (
@@ -55,27 +63,28 @@ class ClassList extends StatelessWidget {
 							textAlign: TextAlign.center,
 						)
 					)
-				)
-			]
-		) + periods.map (
-			(Period period) {
-				final Subject subject = reader.subjects[period.id];
-				final List<String> info = period.getInfo(subject);
-				// ListTile has the period number, so get rid of it
-				info.removeWhere(
-					(String description) => description.startsWith("Period:")
-				);
-				return ClassPanel (
-					title: (
-						"${period.period}"
-						"${int.tryParse (period.period) == null ? "" : ": "}"
-						"${period.getName(subject)}"
-					),
-					children: info.map (
-						(String description) => Text (description)
-					).toList(),
-				);
-			}
-		).toList()
+				),
+			...periods.map (
+				(Period period) {
+					final Subject subject = reader.subjects[period.id];
+					final List<String> info = period.getInfo(subject);
+					// ListTile has the period number, so get rid of it
+					info.removeWhere(
+						(String description) => description.startsWith("Period:")
+					);
+					return ClassPanel (
+						title: (
+							"${period.period}"
+							"${int.tryParse (period.period) == null ? "" : ": "}"
+							"${period.getName(subject)}"
+						),
+						id: period.id,
+						children: info.map (
+							(String description) => Text (description)
+						).toList(),
+					);
+				}
+			).toList()
+		]
 	);
 }
