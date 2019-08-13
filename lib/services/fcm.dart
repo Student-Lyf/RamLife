143d1860f8825d6b6b0a6733ff01584d1e6daf05
Levi Lesches <levilesches@gmail.com>
2019-08-13 19:02:00 -0400

    Registered app for notifications
    Data messages when terminated will be supported soon.
    Check https://github.com/flutter/plugins/pull/1900

--- lib/services/fcm.dart
+++ lib/services/fcm.dart
@@ -1,5 +1,15 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+// So basically, here's the gist with these things
+// When the app is in the foreground, it's onMessage
+// If not, then: 
+// if it's in the background:
+// 	if it's a notification, it's onResume
+// 	otherwise it's a data message and it'll be onMessage
+// if it's terminated: 
+// 	if it's a notification: it's onLaunch
+// 	otherwise it's a data message and it's onMessage
+
 final FirebaseMessaging firebase = FirebaseMessaging();
 
 Future<String> getToken() async => firebase.getToken();
@@ -10,7 +20,7 @@ Future<void> callback(Map<String, dynamic> message) async {
 }
 
 void registerNotifications() => firebase.configure(
-	onMessage: callback, 
-	onResume: callback, 
+	onMessage: callback,
+	onResume: callback,
 	onLaunch: callback
 );
