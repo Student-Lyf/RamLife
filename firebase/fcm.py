a9a28254b022f99512dd6e9212daa228cb1ae218
Levi Lesches <levilesches@gmail.com>
2019-08-13 23:02:30 -0400

    Added a collapse key to FCM notifications

--- firebase/fcm.py
+++ firebase/fcm.py
@@ -3,18 +3,13 @@ init()
 
 from firebase_admin import messaging 
 
-# token = "fYh2TKdxTvE:APA91bEfMClJXMhpdJP2_OSeiQD7P6Q-BNFKKSGz5qEPnm6QmWJWkgfwPyZcZQgw-cEDVAFBOnzHD3em78Yhj9CXyoe1uuunH_o2RgebJLkD1ieXr79-NpF9txkMJ7opTM0DTHDHGev6",
-# token = "267381428578"
 token = "fYh2TKdxTvE:APA91bEfMClJXMhpdJP2_OSeiQD7P6Q-BNFKKSGz5qEPnm6QmWJWkgfwPyZcZQgw-cEDVAFBOnzHD3em78Yhj9CXyoe1uuunH_o2RgebJLkD1ieXr79-NpF9txkMJ7opTM0DTHDHGev6"
 
 message = messaging.Message(
-	notification=messaging.Notification(
-		title = "hello",
-		body = "testing",
-	),
 	data = {
-		"body": "hey there",
+		"command": "refresh",
 		"click_action": "FLUTTER_NOTIFICATION_CLICK",
+		"collapseKey": "refresh",
 	},
 	token = token
 )
