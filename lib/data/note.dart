c7009adb88658ac24c2fc4fb28171fdeedba7abf
Levi Lesches <levilesches@gmail.com>
2019-08-14 00:25:39 -0400

    Added Note dataclass

---
+++ lib/data/note.dart
@@ -0,0 +1,32 @@
+import "package:flutter/foundation.dart" show required;
+
+import "package:ramaz/data/schedule.dart" show Letters, lettersToString;
+
+class Note {
+	final String message, period;
+	final DateTime date;
+	final Letters letter;
+
+	const Note({
+		@required this.message,
+		@required this.period,
+		@required this.letter,
+		this.date,
+	});
+
+	factory Note.fromJson(Map<String, dynamic> json) => Note (
+		message: json ["message"],
+		period: json ["period"],
+		letter: json ["letter"],
+		date: json ["date"],
+	);
+
+	Map<String, dynamic> toJson() => {
+		"message": message,
+		"period": period,
+		"letter": lettersToString [letter],
+		"date": date,
+	};
+
+	bool get repeats => date == null;
+}
