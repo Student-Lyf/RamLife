160a13bf1ea5df5ecf392991f16e4554780653f6
Levi Lesches <levilesches@gmail.com>
2019-08-15 19:27:18 -0400

    Added full notes page

--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -6,10 +6,11 @@ import "package:ramaz/models/notes.dart";
 import "package:ramaz/data/note.dart";
 import "package:ramaz/widgets/change_notifier_listener.dart";
 import "package:ramaz/pages/drawer.dart" show NavigationDrawer;
+import "package:ramaz/pages/notes_builder.dart";
 
 import "package:ramaz/widgets/note_tile.dart";
 
-class NotesPage extends StatelessWidget {
+class NotesPage extends StatefulWidget {
 	final Reader reader;
 	final Preferences prefs;
 
@@ -19,21 +20,45 @@ class NotesPage extends StatelessWidget {
 	});
 
 	@override 
+	NotesPageState createState() => NotesPageState();
+}
+
+class NotesPageState extends State<NotesPage> {
+	NotesPageModel model;
+
+	@override initState() {
+		super.initState();
+		model = NotesPageModel(reader: widget.reader);
+	}
+
+	@override 
 	Widget build(BuildContext context) => ChangeNotifierListener<NotesPageModel>(
-		child: FloatingActionButton(
-			child: Icon (Icons.note_add),
-			onPressed: () => print ("Add new note"),
-		),
-		model: NotesPageModel(reader: reader),
+		model: model,
 		builder: (BuildContext context, NotesPageModel model, Widget child) => Scaffold(
-			drawer: NavigationDrawer(prefs),
+			drawer: NavigationDrawer(widget.prefs),
 			appBar: AppBar(title: Text ("Notes")),
-			floatingActionButton: child,
-			body: ListView (
-				children: model.notes.map(
-					(Note note) => NoteTile(note: note)
-				).toList(),
+			floatingActionButton: FloatingActionButton(
+				child: Icon (Icons.note_add),
+				onPressed: () async => model.saveNote (await showBuilder(context)),
+			),
+			body: ListView.builder (
+				itemCount: model.notes.length,
+				itemBuilder: (BuildContext context, int index) => NoteTile(
+					note: model.notes [index],
+					onTap: () async => model.replace(
+						index, 
+						await showBuilder(context, model.notes [index]),
+					),
+					onDelete: () => model.deleteNote(index),
+				),
 			)
 		)
 	);
+
+	Future<Note> showBuilder(BuildContext context, [Note note]) => 
+		showDialog<Note>(
+			context: context,
+			builder : (BuildContext context) => 
+				NotesBuilder(reader: widget.reader, note: note)
+		);
 }
