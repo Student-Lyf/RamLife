4ab79c8e17007ab3405825deb371d8888a52f9df
Levi Lesches <levilesches@gmail.com>
2019-08-15 19:30:32 -0400

    Added note tile with CRUD

--- lib/widgets/note_tile.dart
+++ lib/widgets/note_tile.dart
@@ -4,14 +4,22 @@ import "package:ramaz/data/note.dart";
 
 class NoteTile extends StatelessWidget {
 	final Note note;
+	final VoidCallback onTap, onDelete;
 
 	const NoteTile({
 		@required this.note,
+		@required this.onTap,
+		@required this.onDelete
 	});
 
 	@override 
 	Widget build (BuildContext context) => ListTile (
 		title: Text (note.message),
-		subtitle: Text (note.repeat.toString()),
+		subtitle: Text (note.repeat?.toString() ?? ""),
+		onTap: onTap,
+		trailing: IconButton (
+			icon: Icon (Icons.remove_circle),
+			onPressed: onDelete,
+		),
 	);
 }
