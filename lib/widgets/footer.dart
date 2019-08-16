c74a9010b5262f7a65aec2bc24ede82bc1f773ea
Levi Lesches <levilesches@gmail.com>
2019-08-16 17:42:00 -0400

    Gave Footer a `bool hasNote` property
    When true, the Footer will display a "click to see note" label and leads to the Home page

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -7,9 +7,12 @@ class Footer extends StatelessWidget {
 
 	final Period period;
 	final Subject subject;
+	final bool note;
+
 	const Footer ({
 		@required this.period,
 		@required this.subject,
+		@required this.note,
 	});
 
 	@override Widget build (BuildContext context) => period == null 
@@ -17,28 +20,32 @@ class Footer extends StatelessWidget {
 		: BottomSheet (
 			enableDrag: false,
 			onClosing: () {},
-			builder: (BuildContext context) => SizedBox (
-				height: 70,
-				child: Align (
-					child: Column (
-						mainAxisAlignment: MainAxisAlignment.center,
-						children: [
-							Text (
-								"Next: ${period.getName (subject)}",
-								textScaleFactor: textScale
-							),
-							Text (
-								(period
-									.getInfo(subject)
-									..removeWhere(
-										(String str) => 
-											str.startsWith("Period: ") || 
-											str.startsWith("Teacher: ")
-									)
-								).join (". "),
-								textScaleFactor: textScale,
-							)
-						]
+			builder: (BuildContext context) => GestureDetector(
+				onTap: !note ? null : () => Navigator.of(context).pushReplacementNamed("home"),
+				child: SizedBox (
+					height: 70,
+					child: Align (
+						child: Column (
+							mainAxisAlignment: MainAxisAlignment.center,
+							children: [
+								Text (
+									"Next: ${period.getName (subject)}",
+									textScaleFactor: textScale
+								),
+								Text (
+									(period
+										.getInfo(subject)
+										..removeWhere(
+											(String str) => 
+												str.startsWith("Period: ") || 
+												str.startsWith("Teacher: ")
+										)
+									).join (". "),
+									textScaleFactor: textScale,
+								),
+								if (note) Text ("Click to see note"),
+							]
+						)
 					)
 				)
 			)
