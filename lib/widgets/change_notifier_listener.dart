7c8bd6fb65c9663481780914167366c2c01ca09a
Levi Lesches <levilesches@gmail.com>
2019-08-15 23:07:26 -0400

    Added an initState parameter to ChangeNotifierListener

--- lib/widgets/change_notifier_listener.dart
+++ lib/widgets/change_notifier_listener.dart
@@ -3,15 +3,15 @@ import "package:flutter/material.dart";
 typedef ModelBuilder<T extends ChangeNotifier> = Widget Function(BuildContext, T, Widget);
 
 class ChangeNotifierListener<Model extends ChangeNotifier> extends StatefulWidget {
-  final Model model;
-  // final ValueWidgetBuilder<Model> builder;
-  // final ModelBuilder<Model> builder;
+  final Model Function() model;
   final Widget Function(BuildContext, Model, Widget) builder;
   final Widget child;
+  final VoidCallback setup;
   const ChangeNotifierListener ({
     @required this.model,
     @required this.builder,
     this.child,
+    this.setup,
   });
 
   @override 
@@ -19,27 +19,23 @@ class ChangeNotifierListener<Model extends ChangeNotifier> extends StatefulWidge
 }
 
 class ChangeNotifierState<Model extends ChangeNotifier> extends State<ChangeNotifierListener<Model>> {
+  Model model;
+
   @override void initState() {
     super.initState();
-    widget.model.addListener(listener);
-  }
-
-  @override void didUpdateWidget (ChangeNotifierListener<Model> old) {
-    if (old.model != widget.model) {
-      old.model.dispose();
-      widget.model.addListener(listener);
-    }
-    super.didUpdateWidget(old);
+    model = widget.model();
+    model.addListener(listener);
+    widget?.setup();
   }
 
   @override void dispose() {
-    widget.model.removeListener(listener);
-    widget.model.dispose();
+    model.removeListener(listener);
+    model.dispose();
     super.dispose();
   }
 
   @override Widget build (BuildContext context) => widget.builder (
-    context, widget.model, widget.child
+    context, model, widget.child
   );
 
   void listener() => setState(() {});
