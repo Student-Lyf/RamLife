61b189098d9fe32cecad5e09762c05cf8cea5ba8
Levi Lesches <levilesches@gmail.com>
2019-08-16 00:07:33 -0400

    Modified class list view
    Now takes in a Day instead of a list of periods
    
    Also, Class Panel can now show multiple notes in a single panel

--- lib/widgets/class_list.dart
+++ lib/widgets/class_list.dart
@@ -1,39 +1,50 @@
 import "package:flutter/material.dart";
 
-import "package:ramaz/data/schedule.dart" show Period, Subject;
+import "package:ramaz/data/schedule.dart" show Period, Subject, Day;
+import "package:ramaz/data/note.dart";
 import "package:ramaz/services/reader.dart";
+import "package:ramaz/widgets/note_tile.dart";
 
 class ClassPanel extends StatelessWidget {
 	final String title, id;
-	final List <Widget> children;
+	final List<Widget> children;
+	final List<Note> notes;
 
 	const ClassPanel ({
 		@required this.title,
 		@required this.children,
 		@required this.id,
+		this.notes,
 	});
 
 	@override Widget build (BuildContext context) => ExpansionTile (
-		// title: ListTile (
-		// 	title: Text (title),
-		// 	subtitle: Text (id ?? ""),
-		// 	contentPadding: EdgeInsets.symmetric(vertical: 0),
-		// ),
 		title: Text (title),
 		children: [
-			Align (
-				alignment: const Alignment (-0.75, 0),
-				child: Column (
-					crossAxisAlignment: CrossAxisAlignment.start,
-					children: [
-						...children, 
-						if (id != null) Text ("ID: $id")
-					].map (
-						(Widget child) => Padding (
-							padding: const EdgeInsets.symmetric(vertical: 5),
-							child: child
-						) 
-					).toList()
+			Padding (
+				padding: EdgeInsets.only(left: 30),
+				child: Align (
+					alignment: Alignment.centerLeft,					
+					child: Column (
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							...children, 
+							if (id != null) Text ("ID: $id")
+						].map<Widget> (
+							(Widget child) => Padding (
+								padding: const EdgeInsets.symmetric(vertical: 5),
+								child: child
+							) 
+						).toList() + (
+							notes.map(
+								(Note note) => NoteTile (
+									note: note,
+									onTap: () {},
+									onDelete: () {},
+									height: 65,
+								)
+							).toList()
+						)
+					)
 				)
 			)
 		]
@@ -42,14 +53,17 @@ class ClassPanel extends StatelessWidget {
 
 
 class ClassList extends StatelessWidget {
-	final Iterable<Period> periods;
+	final Day day;
+	final Iterable<Period> _periods;
 	final String headerText;
 	final Reader reader;
+
 	ClassList ({
-		@required this.periods, 
+		@required this.day, 
 		@required this.reader,
-		this.headerText
-	});
+		this.headerText,
+		Iterable periods,
+	}) : _periods = periods ?? reader.student.getPeriods(day);
 
 	@override Widget build (BuildContext context) => ListView (
 		shrinkWrap: true,
@@ -64,7 +78,7 @@ class ClassList extends StatelessWidget {
 						)
 					)
 				),
-			...periods.map (
+			..._periods.map (
 				(Period period) {
 					final Subject subject = reader.subjects[period.id];
 					final List<String> info = period.getInfo(subject);
@@ -72,7 +86,15 @@ class ClassList extends StatelessWidget {
 					info.removeWhere(
 						(String description) => description.startsWith("Period:")
 					);
+					final List<Note> notes = reader.notes.where(
+						(Note note) => note.repeat.doesApply(
+							period: period.period,
+							letter: day.letter,
+							subject: subject,
+						)
+					).toList();
 					return ClassPanel (
+						notes: notes,
 						title: (
 							"${period.period}"
 							"${int.tryParse (period.period) == null ? "" : ": "}"
