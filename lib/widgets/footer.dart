037d24fd92830995c562650e054feb068b720d0f
Levi Lesches <levilesches@gmail.com>
2019-08-19 01:34:13 -0400

    Made Footer fetch Services

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,53 +1,51 @@
 import "package:flutter/material.dart";
 
-import "package:ramaz/data/schedule.dart";
+import "package:ramaz/widgets/services.dart";
+import "package:ramaz/widgets/change_notifier_listener.dart";
+import "package:ramaz/services/schedule.dart";
 
 class Footer extends StatelessWidget {
 	static const double textScale = 1.25;
 
-	final Period period;
-	final Subject subject;
-	final bool note;
-
-	const Footer ({
-		@required this.period,
-		@required this.subject,
-		@required this.note,
-	});
-
-	@override Widget build (BuildContext context) => period == null 
-		? Container(height: 0, width: 0) 
-		: BottomSheet (
-			enableDrag: false,
-			onClosing: () {},
-			builder: (BuildContext context) => GestureDetector(
-				onTap: !note ? null : () => Navigator.of(context).pushReplacementNamed("home"),
-				child: SizedBox (
-					height: 70,
-					child: Align (
-						child: Column (
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-								Text (
-									"Next: ${period.getName (subject)}",
-									textScaleFactor: textScale
-								),
-								Text (
-									(period
-										.getInfo(subject)
-										..removeWhere(
-											(String str) => 
-												str.startsWith("Period: ") || 
-												str.startsWith("Teacher: ")
-										)
-									).join (". "),
-									textScaleFactor: textScale,
-								),
-								if (note) Text ("Click to see note"),
-							]
+	@override Widget build (BuildContext context) => ChangeNotifierListener(
+			model: () => Services.of(context).schedule,
+			dispose: false,
+			child: Container(height: 0, width: 0),
+			builder: (BuildContext context, Schedule schedule, Widget blank) =>
+				schedule.period == null ? blank : BottomSheet (
+					enableDrag: false,
+					onClosing: () {},
+					builder: (BuildContext context) => GestureDetector(
+						onTap: !schedule.notes.hasNote ? null : 
+							() => Navigator.of(context).pushReplacementNamed("home"),
+						child: SizedBox (
+							height: 70,
+							child: Align (
+								child: Column (
+									mainAxisAlignment: MainAxisAlignment.center,
+									children: [
+										Text (
+											"Next: ${schedule.period.getName (schedule.subject)}",
+											textScaleFactor: textScale
+										),
+										Text (
+											(schedule.period
+												.getInfo(schedule.subject)
+												..removeWhere(
+													(String str) => (
+														str.startsWith("Period: ") || 
+														str.startsWith("Teacher: ")
+													)
+												)
+											).join (". "),
+											textScaleFactor: textScale,
+										),
+										if (schedule.notes.hasNote) Text ("Click to see note"),
+									]
+								)
+							)
 						)
 					)
 				)
-			)
-		);
+			);
 }
