4c13a6096e8cb67d31562c852db2c631d89cfa8b
Levi Lesches <levilesches@gmail.com>
2019-08-19 01:34:52 -0400

    Made NoteTile listen for changes to notes

--- lib/widgets/note_tile.dart
+++ lib/widgets/note_tile.dart
@@ -2,16 +2,18 @@ import "package:flutter/material.dart";
 
 import "package:ramaz/data/note.dart";
 
-class NoteTile extends StatelessWidget {
-	final Note note;
-	final VoidCallback onTap, onDelete;
+import "package:ramaz/services/notes.dart";
 
+import "package:ramaz/widgets/services.dart";
+import "package:ramaz/widgets/change_notifier_listener.dart";
+import "package:ramaz/pages/notes_builder.dart";
+
+class NoteTile extends StatelessWidget {
+	final int index;
 	final double height;
 
 	const NoteTile({
-		@required this.note,
-		@required this.onTap,
-		@required this.onDelete,
+		@required this.index,
 		this.height = 65,
 	});
 
@@ -19,14 +21,28 @@ class NoteTile extends StatelessWidget {
 	Widget build (BuildContext context) => SizedBox (
 		height: height, 
 		child: Center (
-			child: ListTile (
-				title: Text (note.message),
-				subtitle: Text (note.repeat?.toString() ?? ""),
-				onTap: onTap,
-				trailing: IconButton (
-					icon: Icon (Icons.remove_circle, color: Theme.of(context).iconTheme.color),
-					onPressed: onDelete,
+			child: ChangeNotifierListener(
+				model: () => Services.of(context).notes,
+				dispose: false,
+				child: Icon (
+					Icons.remove_circle, 
+					color: Theme.of(context).iconTheme.color
 				),
+				builder: (BuildContext context, Notes notes, Widget icon) {
+					final Note note = notes.notes [index];
+					return ListTile(
+						title: Text (note.message),
+						subtitle: Text (note.repeat?.toString() ?? ""),
+						onTap: () async => notes.replaceNote(
+							index, 
+							await NotesBuilder.buildNote(context, note),
+						),
+						trailing: IconButton (
+							icon: icon,
+							onPressed: () => notes.deleteNote(index),
+						),
+					);
+				}
 			),
 		),
 	);
