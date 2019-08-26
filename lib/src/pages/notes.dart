7168c1ad5f1dd74ec277fd5f4a5f3fe0ad1c6cc4
Levi Lesches <levilesches@gmail.com>
2019-08-26 01:24:24 -0400

    Made NoteTile not listen for changes
    The logic here is that the parent widget should always be listening

--- lib/src/pages/notes.dart
+++ lib/src/pages/notes.dart
@@ -8,8 +8,15 @@ class NotesPage extends StatelessWidget {
 	@override 
 	Widget build(BuildContext context) => ChangeNotifierListener<Notes>(
 		model: () => Services.of(context).notes,
+		child: Center (
+			child: Text (
+				"You don't have any notes yet",
+				textScaleFactor: 1.5,
+				textAlign: TextAlign.center,
+			),
+		),
 		dispose: false,
-		builder: (BuildContext context, Notes model, _) => Scaffold(
+		builder: (BuildContext context, Notes model, Widget none) => Scaffold(
 			bottomNavigationBar: Footer(),
 			drawer: NavigationDrawer(),
 			appBar: AppBar(
@@ -27,13 +34,7 @@ class NotesPage extends StatelessWidget {
 					model.addNote(await NotesBuilder.buildNote(context)),
 			),
 			body: model.notes.isEmpty
-				? Center (
-					child: Text (
-						"You don't have any notes yet",
-						textScaleFactor: 1.5,
-						textAlign: TextAlign.center,
-					),
-				) 
+				? none 
 				: ListView.separated (
 					itemCount: model.notes.length,
 					separatorBuilder: (_, __) => Divider(),
