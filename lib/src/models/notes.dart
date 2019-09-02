69f04167d0cca6c2e82a589f375f622c13dc4d06
Levi Lesches <levilesches@gmail.com>
2019-09-02 19:06:07 -0400

    Randomly switching map to list comp

--- lib/src/models/notes.dart
+++ lib/src/models/notes.dart
@@ -14,8 +14,10 @@ class Notes with ChangeNotifier {
 	void setup() {
 		final Map<String, dynamic> data = reader.notesData;
 		readNotes = List<int>.from(data ["read"] ?? []);
-		notes = data ["notes"]
-			?.map<Note>((json) => Note.fromJson(json))?.toList() ?? [];
+		notes = [
+			for (final json in data ["notes"] ?? [])
+				Note.fromJson(json)
+		];
 		notifyListeners();
 	}
 
