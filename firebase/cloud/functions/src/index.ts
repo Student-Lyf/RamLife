fb2f1d3c54c40dc8780d7aae297a19d5c70194f9
Levi Lesches <levilesches@gmail.com>
2019-09-08 20:11:31 -0400

    Fixed cloud function

--- firebase/cloud/functions/src/index.ts
+++ firebase/cloud/functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const calendarUpdated = functions
+	.firestore
+	.document("/calendar/{month}")
+	.onUpdate(
+		async (snapshot, context) => {
+			const payload = {
+				data: {command: "updateCalendar"}
+			}
+
+			console.log("Calendar updated")
+
+			await admin
+				.messaging()
+				.sendToTopic(
+					"calendar", 
+					payload
+				)
+		}
+	)
