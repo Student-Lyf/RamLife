2408b0d9e2bdd7378bb088f71a4ceaf4a06a0cc4
Levi Lesches <levilesches@gmail.com>
2019-09-09 21:46:53 -0400

    Added PublicationsModel

--- lib/models.dart
+++ lib/models.dart
@@ -2,5 +2,6 @@ library models;
 
 export "src/models/notes.dart";
 export "src/models/notes_builder.dart";
+export "src/models/publications.dart";
 export "src/models/schedule.dart";
 export "src/models/schedule_page.dart";
