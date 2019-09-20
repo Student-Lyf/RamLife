860c30ac2bee5da01177d515e38d3cf095887e50
Levi Lesches <levilesches@gmail.com>
2019-09-20 15:28:19 -0400

    Made myself owner of Rampage

--- firebase/admins.py
+++ firebase/admins.py
@@ -3,11 +3,15 @@ init()
 from firebase_admin import auth as Firebase
 
 CALENDAR = "calendar"
+PUBLICATIONS = "publications"
 
 ADMINS = {
-	"leschesl@ramaz.org": [CALENDAR],
+	"leschesl@ramaz.org": [CALENDAR, PUBLICATIONS],
 }
 
 for email, scopes in ADMINS.items():
 	user = Firebase.get_user_by_email(email)
-	Firebase.set_custom_user_claims(user.uid, {'scopes': scopes})
+	Firebase.set_custom_user_claims(
+		user.uid, 
+		{'scopes': scopes, "club": "Rampage"}
+	)
