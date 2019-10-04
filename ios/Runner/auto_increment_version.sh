a7f34309aeb7cae0db53e2e779fada7d0a8a6500
Levi Lesches <levilesches@Levis-iMac.local>
2019-10-03 19:32:27 -0700

    Added some fixes for iOS signing

---
+++ ios/Runner/auto_increment_version.sh
@@ -0,0 +1,3 @@
+buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+buildNumber=$((buildNumber + 1))
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
