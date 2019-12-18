572ce52ae31b16c248f849c7e1481fe9db8a1fd6
Levi Lesches <levilesches@gmail.com>
2019-12-18 16:14:02 -0500

    Converted Footer from current to next period

--- lib/src/widgets/generic/footer.dart
+++ lib/src/widgets/generic/footer.dart
@@ -11,52 +11,54 @@ class Footer extends StatelessWidget {
 	Footer();
 
 	@override Widget build (BuildContext context) => ModelListener<Schedule>(
-			model: () => Services.of(context).schedule,
-			dispose: false,
-			// ignore: sort_child_properties_last
-			child: Container(height: 0, width: 0),
-			builder: (BuildContext context, Schedule schedule, Widget blank) =>
-				schedule.period == null ? blank : BottomSheet (
-					enableDrag: false,
-					onClosing: () {},
-					builder: (BuildContext context) => GestureDetector(
-						onTap: !schedule.reminders.hasReminder ? null : 
-							() {
-								final NavigatorState nav = Navigator.of(context);
-								if (nav.canPop()) {
-									nav.pop();
-								}
-								nav.pushReplacementNamed(Routes.home);
-							},
-						child: SizedBox (
-							height: 70,
-							child: Align (
-								child: Column (
-									mainAxisAlignment: MainAxisAlignment.center,
-									children: [
-										Text (
-											"Next: ${schedule.period.getName (schedule.subject)}",
-											textScaleFactor: textScale
-										),
-										Text (
-											(schedule.period
-												.getInfo(schedule.subject)
-												..removeWhere(
-													(String str) => (
-														str.startsWith("Period: ") || 
-														str.startsWith("Teacher: ")
-													)
+		model: () => Services.of(context).schedule,
+		dispose: false,
+		// ignore: sort_child_properties_last
+		child: Container(height: 0, width: 0),
+		builder: (BuildContext context, Schedule schedule, Widget blank) =>
+			schedule.nextPeriod == null ? blank : BottomSheet (
+				enableDrag: false,
+				onClosing: () {},
+				builder: (BuildContext context) => GestureDetector(
+					onTap: !schedule.reminders.hasReminder ? null : 
+						() {
+							final NavigatorState nav = Navigator.of(context);
+							if (nav.canPop()) {
+								nav.pop();
+							}
+							nav.pushReplacementNamed(Routes.home);
+						},
+					child: SizedBox (
+						height: 70,
+						child: Align (
+							child: Column (
+								mainAxisAlignment: MainAxisAlignment.center,
+								children: [
+									Text (
+										"Next: ${schedule.nextPeriod.getName(schedule.nextSubject)}",
+										textScaleFactor: textScale
+									),
+									Text (
+										(schedule.nextPeriod
+											.getInfo(schedule.nextSubject)
+											..removeWhere(
+												(String str) => (
+													str.startsWith("Period: ") || 
+													str.startsWith("Teacher: ")
 												)
-											).join (". "),
-											textScaleFactor: textScale,
-										),
-										if (schedule.reminders.hasReminder) 
-											const Text ("Click to see reminder"),
-									]
-								)
+											)
+										).join (". "),
+										textScaleFactor: textScale,
+									),
+									if (schedule.nextPeriod.activity != null) 
+										const Text("There is an activity"),
+									if (schedule.reminders.hasReminder) 
+										const Text ("Click to see reminder"),
+								]
 							)
 						)
 					)
 				)
-			);
+			)
+		);
 }
