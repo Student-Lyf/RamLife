9b34ca57d4535af17fc2d3486d0e5ee73e070bf5
Levi Lesches <levilesches@gmail.com>
2020-01-07 12:04:11 -0500

    Documented admin data models.

--- lib/src/data/admin.dart
+++ lib/src/data/admin.dart
@@ -2,34 +2,52 @@ import "package:flutter/foundation.dart";
 
 import "times.dart";
 
-enum Scope {calendar, schedule}
+/// Scopes for administrative privileges.
+/// 
+/// [Admin] users use these scopes ([Admin.scopes]) to determine what they can 
+/// access and/or modify.
+enum Scope {
+	/// The admin can access and modify the calendar.
+	calendar, 
 
+	/// The admin can access and modify student schedules.
+	schedule
+}
+
+/// Maps Strings to their respective [Scope]s.
 const Map<String, Scope> stringToScope = {
 	"calendar": Scope.calendar,
 	"schedule": Scope.schedule,
 };
 
+/// Maps [Scope]s to Strings. 
 const Map<Scope, String> scopeToString = {
 	Scope.calendar: "calendar",
 	Scope.schedule: "schedule",
 };
 
+/// A system administrator. 
+/// 
+/// Based on the scopes granted to them, they can 
+/// access and/or modify data not normally visible/modifiable to all users.
 @immutable
 class Admin {
-	final String email, name;
+	/// A list of scopes available to this user. 
 	final List<Scope> scopes;
+
+	/// A list of custom-made [Special]s by this admin. 
+	/// 
+	/// These can be saved so the admin does not have to recreate them. 
 	final List<Special> specials;
 
+	/// Creates a user with administrative privileges. 
 	const Admin ({
-		this.email, 
-		this.name, 
 		this.scopes, 
 		this.specials
 	});
 
+	/// Creates an admin from a JSON entry. 
 	Admin.fromJson(Map<String, dynamic> json) :
-		email = json ["email"],
-		name = json ["name"],
 		scopes = [
 			for (dynamic scope in json ["scopes"])
 				stringToScope [scope]
@@ -39,9 +57,8 @@ class Admin {
 				Special.fromJson (Map<String, dynamic>.from(special))
 		];
 
+	/// Converts an admin to JSON form. 
 	Map<String, dynamic> toJson() => {
-		"email": email,
-		"name": name,
 		"scopes": [
 			for (final Scope scope in scopes)
 				scopeToString [scope]
--- lib/src/models/data/admin.dart
+++ lib/src/models/data/admin.dart
@@ -6,12 +6,19 @@ import "admin/calendar.dart";
 export "admin/admin.dart";
 export "admin/calendar.dart";
 
+/// A data model that manages all admin responsibilities. 
 class AdminModel {
 	CalendarModel _calendar;
+	
+	/// The admin user this model is managing. 
+	/// 
+	/// This is an [AdminUserModel], not just the user itself. 
 	AdminUserModel user;
 
+	/// Creates an admin data model. 
 	AdminModel(ServicesCollection services) : 
 		user = AdminUserModel(services.reader);
 
+	/// The data model for modifying the calendar. 
 	CalendarModel get calendar => _calendar ??= CalendarModel();
 }
