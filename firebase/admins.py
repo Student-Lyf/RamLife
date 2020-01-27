f3f97321a3d8da83885295fc28bde6e87342fc0f
Levi Lesches <levilesches@gmail.com>
2020-01-27 17:25:10 -0500

    Added admin:bool scope to Firebase custom claims

--- firebase/admins.py
+++ firebase/admins.py
@@ -13,5 +13,5 @@ for email, scopes in ADMINS.items():
 	user = Firebase.get_user_by_email(email)
 	Firebase.set_custom_user_claims(
 		user.uid, 
-		{'scopes': scopes, "club": "Rampage"}
+		{"admin": True, 'scopes': scopes, "club": "Rampage"}
 	)
