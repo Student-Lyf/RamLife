5c17fccfc365aa9ecd249113cb6d44c017d03c35
Levi Lesches <levilesches@gmail.com>
2020-03-18 13:53:16 -0400

    Fixed main.py for being moved to another folder

--- firebase_py/main.py
+++ firebase_py/main.py
@@ -3,8 +3,8 @@ from firebase_admin import initialize_app, credentials
 from pathlib import Path
 
 cd = Path.cwd()
-if cd.name != "firebase":   # must be in root directory
-	cd /= "firebase"
+if cd.name != "firebase_py":   # must be in root directory
+	cd /= "firebase_py"
 
 path = str (cd / "admin.json")
 
