bebc77e842537a1a652077f42902160c03dec9ad
Levi Lesches <levilesches@gmail.com>
2020-03-24 15:04:03 -0400

    Documented sections

--- firebase/firestore/lib/src/sections/logic.dart
+++ firebase/firestore/lib/src/sections/logic.dart
@@ -1,11 +1,28 @@
 import "package:firestore/data.dart";
 
+import "reader.dart";  // for doc comments
+
+/// A collection of functions to index course data. 
+/// 
+/// No function in this class reads data from the data files, just works logic
+/// on them. This helps keep the program modular, by separating the data sources
+/// from the data indexing.
 class SectionLogic {
+	/// Converts a section ID to a course ID.
 	static String getCourseId(String sectionId) {
 		final String result = sectionId.substring(0, sectionId.indexOf("-"));
 		return result.startsWith("0") ? result.substring(1) : result;
 	}
 
+	/// Builds a list of [Section] objects.
+	/// 
+	/// This function works by taking several arguments: 
+	/// 
+	/// - courseNames, from [SectionReader.courseNames]
+	/// - sectionTeachers, from [SectionReader.getSectionTeachers] (`id: false`)
+	/// 
+	/// These are kept as parameters instead of calling the functions by itself
+	/// in order to keep the data and logic layers separate. 
 	static List<Section> getSections({
 		@required Map<String, String> courseNames,
 		@required Map<String, String> sectionTeachers,
--- firebase/firestore/lib/src/sections/reader.dart
+++ firebase/firestore/lib/src/sections/reader.dart
@@ -1,12 +1,22 @@
 import "package:firestore/helpers.dart";
 
+/// A collection of functions to read course data.
+/// 
+/// No function in this class actually performs logic on said data, just returns
+/// it. This helps keep the program modular, by separating the data sources from
+/// the data indexing.
 class SectionReader {
+	/// Maps course IDs to their respective names.
 	static Future<Map<String, String>> get courseNames async => {
 		await for (final Map<String, String> row in csvReader(DataFiles.courses))
 			if (row ["SCHOOL_ID"] == "Upper")
 				row ["ID"]: row ["FULL_NAME"]
 	};
 
+	/// Maps section IDs to their respective teachers. 
+	/// 
+	/// If [id] is true, the values are the faculty IDs. Otherwise, the values are
+	/// the teachers' full names.
 	static Future<Map<String, String>> getSectionTeachers({
 		bool id = false
 	}) async {
