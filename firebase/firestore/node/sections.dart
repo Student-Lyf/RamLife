02a173494c291f5ffc4ab736702d827e9f5469bf
Levi Lesches <levilesches@gmail.com>
2020-03-25 11:58:27 -0400

    Added debug logging to node/sections.dart

--- firebase/firestore/node/sections.dart
+++ firebase/firestore/node/sections.dart
@@ -9,21 +9,30 @@ Future<void> main() async {
 	final List args = process.argv.sublist(2);  // needed for Node.js
 	final bool upload = args.contains("--upload");	
 	final bool verbose = {"--verbose", "-v"}.any(args.contains);
-	if (verbose) {
+	final bool debug = {"--debug", "-d"}.any(args.contains);
+	if (debug) {
+		Logger.level = LogLevel.debug;
+	} else if (verbose) {
 		Logger.level = LogLevel.verbose;
 	}
 	Logger.debug("Upload: $upload.");
 	Logger.info("Indexing data...");
 
+	Logger.debug("Getting course names");
 	final Map<String, String> courseNames = await SectionReader.courseNames;
+	Logger.verbose("Names: $courseNames.");
 
+	Logger.debug("Getting teachers for each section");
 	final Map<String, String> sectionTeachers = 
 		await SectionReader.getSectionTeachers();
+	Logger.verbose("Teachers: $sectionTeachers.");
 
+	Logger.debug("Building list of sections");
 	final List<Section> sections = SectionLogic.getSections(
 		courseNames: courseNames,
 		sectionTeachers: sectionTeachers,
 	);
+	Logger.verbose("Sections: $sections");
 
 	Logger.info("Finished data indexing.");
 
