ff3bb219d24c850a495b36c7fb8ca29cdb8496f8
Levi Lesches <levilesches@gmail.com>
2020-03-25 17:51:08 -0400

    Made build the default for main.bat

--- firebase/firestore/main.bat
+++ firebase/firestore/main.bat
@@ -4,18 +4,17 @@ if [%1]==[] goto helpMessage
 if "%1"=="/?" goto helpMessage
 if "%1"=="-h" goto helpMessage
 
-set build=0
-if "%1"=="-b" set build=1
-if "%1"=="--build" set build=1
+set run=0
+if "%1"=="-r" set run=1
+if "%1"=="--run" set run=1
 
-if "%build%"=="1" shift /1
+if "%run%"=="1" shift /1  rem Remove the -r 
 
 set filename=%1.dart.js
 set args=%2 %3 %4 %5 %6 %7 %8 %9
 set runCommand=node build\%filename% %args%
 
-
-if "%build%"=="1" (
+if "%run%"=="0" (
 	webdev build --no-build-web-compilers --output "node:build"
 	echo.
 	%runCommand%
@@ -29,8 +28,8 @@ exit /b
 :helpMessage
 echo.
 echo A helper script to compile dart files to Node.js
-echo Usage: main [--build ^| -b] filename [args]
+echo Usage: main [--run ^| -r] filename [args]
 echo.
-echo build: Use dart2js to rebuild the dart files
+echo run: Run the .js script instead of using dart2js to rebuild the dart files (defaults to build)
 echo filename: The name of the dart file to run. The file should be node/{filename}.dart
 echo args: The arguments to pass to the built .js file
