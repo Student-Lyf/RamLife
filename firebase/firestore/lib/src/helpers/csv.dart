ea4a62fd99b0fb039f0a7205860e3af6ad6a4bf5
Levi Lesches <levilesches@gmail.com>
2020-03-26 02:35:12 -0400

    Expanded csv

--- firebase/firestore/lib/src/helpers/csv.dart
+++ firebase/firestore/lib/src/helpers/csv.dart
@@ -1,24 +1,39 @@
 import "dir.dart";
 
-/// An iterator over a CSV file. 
+/// An iterator over a CSV file with headers
 /// 
 /// Assuming the first row is the header row, every iteration returns a 
 /// `Map<String, String>`, where the keys are the headers and the values are 
-/// the values of the current row. 
+/// the values of the current row. The values are read from [csvReadLines].
 /// 
 /// Since this function uses the async file reading methods, it returns [Stream]
 /// instead of [Iterable]. To use it in a for loop, instead of using `for`, use
 /// `await for` (and make the function async).
 Stream<Map<String, String>> csvReader(String filename) async* {
-	final File file = File(filename);
-	final List<String> lines = await file.readAsLines();
+	// Needs to be broadcast so we can use [Stream.first]
 	List<String> headers;
-	for (int index = 0; index < lines.length; index++) {
-		List<String> contents = lines [index].split(",");
-		if (index == 0) {
+	await for(
+		final List<String> contents in 
+		csvReadLines(filename)
+	) {
+		if (headers == null) {
 			headers = contents;
 			continue;
-		}
+		} 
+		yield Map.fromIterables(headers, contents);
+	}
+}
+
+/// Iterates over a CSV file and returns their contents.
+/// 
+/// Since this function uses the async file reading methods, it returns [Stream]
+/// instead of [Iterable]. To use it in a for loop, instead of using `for`, use
+/// `await for` (and make the function async).
+Stream<List<String>> csvReadLines(String filename) async* {
+	final File file = File(filename);
+	final List<String> lines = await file.readAsLines();
+	for (int index = 0; index < lines.length; index++) {
+		List<String> contents = lines[index].split(",");
 
 		// If a comma is inside quotes, join them together
 		int startIndex, endIndex;
@@ -31,10 +46,12 @@ Stream<Map<String, String>> csvReader(String filename) async* {
 				break;
 			}
 		}
+
 		assert(
 			(startIndex == null) == (endIndex == null), 
 			"Cannot parse CSV line $index in $filename: $contents"
 		);
+
 		if (startIndex != null && endIndex != null) {
 			contents = [
 				...contents.sublist(0, startIndex),
@@ -42,6 +59,6 @@ Stream<Map<String, String>> csvReader(String filename) async* {
 				...contents.sublist(endIndex)
 			];
 		}
-		yield Map.fromIterables(headers, contents);
+		yield contents;
 	}
 }
