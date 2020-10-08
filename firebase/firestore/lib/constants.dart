97ba776d483a71f52ddc9deafd464f9d5686986c
Levi Lesches <levilesches@gmail.com>
2020-10-08 00:35:06 -0400

    Added constants.yaml

--- firebase/firestore/lib/constants.dart
+++ firebase/firestore/lib/constants.dart
@@ -1,7 +1,14 @@
-final Set<String> dayNames = {
-	"Monday", 
-	"Tuesday", 
-	"Wednesday", 
-	"Thursday", 
-	"Friday"
-};
+import "package:yaml/yaml.dart";
+
+import "package:firestore/helpers.dart";
+import "package:node_io/node_io.dart";
+
+final File file = File(DataFiles.constants);
+
+final YamlMap yamlContents = loadYaml(file.readAsStringSync());
+
+final Set<String> dayNames = Set.from(yamlContents ["dayNames"]);
+
+final Set<String> corruptStudents = Set.from(yamlContents ["corruptStudents"]);
+
+final bool isSemester1 = DateTime.now().month > 7;
