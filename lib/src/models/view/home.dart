4d5c894de8f9871ac570805e7b7be709d228b97a
Levi Lesches <levilesches@gmail.com>
2020-10-14 09:59:02 -0400

    Reinstated HomeModel, properly this time

---
+++ lib/src/models/view/home.dart
@@ -0,0 +1,19 @@
+import "package:flutter/foundation.dart";
+
+import "package:ramaz/models.dart";
+
+// ignore: prefer_mixin
+class HomeModel with ChangeNotifier {
+	// Do NOT use a list here. See null checks in [dispose].
+	HomeModel() {
+		Models.schedule.addListener(notifyListeners);
+		Models.sports.addListener(notifyListeners);
+	}
+
+	@override
+	void dispose() {
+		Models?.schedule?.dispose();
+		Models?.sports?.dispose();
+		super.dispose();
+	}
+}
