c01135586a12b629cc313d765896fb8752f2a7f8
todesj <josht2004@gmail.com>
2021-04-21 11:07:50 -0400

    Added schedule search model

---
+++ lib/src/models/view/schedule_search.dart
@@ -0,0 +1,36 @@
+import "package:ramaz/data.dart";
+import "package:ramaz/models.dart";
+
+extension on Subject{
+  String get id => "";
+}
+class ScheduleSearchModel {
+  late List<Subject> subjects;
+
+  scheduleSearchModel() {
+    final ScheduleModel model = Models.instance.schedule;
+    subjects = model.subjects.values.toList();
+  }
+
+  List<Subject> getMatchingClasses(String query) =>
+      [
+        for(final Subject subject in subjects)
+          if(subject.name.contains(query))
+            subject
+      ];
+  List<String> getPeriods(Subject subject){
+    final List<String> result = [];
+    Map<String,List<PeriodData?>> schedule = Models.instance.schedule.user.schedule;
+    for(final MapEntry<String,List<PeriodData?>> entry in schedule.entries){
+      final String dayName = entry.key;
+      final List<PeriodData?> daySchedule = entry.value;
+      for(int index=0;index<daySchedule.length;index++){
+        final PeriodData? period = daySchedule[index];
+        if(period!=null && period.id == subject.id){
+          result.add("$dayName-$index");
+        }
+      }
+    }
+    return result;
+  }
+}
