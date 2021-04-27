c000ba1260cc1fb2d2cf4a7954f5f0c07fedd04e
Levi Lesches <levilesches@gmail.com>
2021-04-27 11:59:31 -0400

    Use cloud_firestore import from services/firestore.dart

--- lib/src/services/databases/schedule/cloud.dart
+++ lib/src/services/databases/schedule/cloud.dart
@@ -1,5 +1,3 @@
-import "package:cloud_firestore/cloud_firestore.dart";
-
 import "../../firestore.dart";
 
 import "interface.dart";
--- lib/src/services/databases/user/cloud.dart
+++ lib/src/services/databases/user/cloud.dart
@@ -1,5 +1,3 @@
-import "package:cloud_firestore/cloud_firestore.dart";
-
 import "../../auth.dart";
 import "../../firestore.dart";
 
