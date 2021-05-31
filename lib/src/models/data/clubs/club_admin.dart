c943c02037f512b7d7f15549a87a1eda470b6bd7
todesj <70974284+todesj@users.noreply.github.com>
2021-05-31 10:55:53 -0400

    Update lib/src/models/data/clubs/club_admin.dart
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

--- lib/src/models/data/clubs/club_admin.dart
+++ lib/src/models/data/clubs/club_admin.dart
@@ -19,7 +19,7 @@ class ClubAdmins extends Model{
 
   /// Allows a clubs admin to delete a club.
   Future<void> deleteClub(Club club) async{
-    Services.instance.database.clubs.delete(club.toJson());
+    Services.instance.database.clubs.delete(club.id);
   }
 
   Future<void> addFacultyAdvisor(Club club, ContactInfo facultyAdvisor){
