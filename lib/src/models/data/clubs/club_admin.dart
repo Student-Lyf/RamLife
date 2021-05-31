21acb05c3cd9df3c7a3d3ea3b8cc2d1e951f2f47
todesj <josht2004@gmail.com>
2021-05-31 01:20:47 -0400

    Fixes syntax error

--- lib/src/models/data/clubs/club_admin.dart
+++ lib/src/models/data/clubs/club_admin.dart
@@ -15,7 +15,7 @@ class ClubAdmins extends Model{
   /// Allows clubs admin to approve a club to be shown to all the users
   Future<void> approveClub(Club club) async{
     club.isApproved=true;
-    Services.instance.database.clubs.aprove(club.id, club.isApproved);
+    Services.instance.database.clubs.save(club.id, club.toJson());
   }
 
   /// Allows a clubs admin to delete a club.
@@ -25,6 +25,6 @@ class ClubAdmins extends Model{
 
   Future<void> addFacultyAdvisor(Club club, ContactInfo facultyAdvisor){
     club.facultyAdvisor.add(facultyAdvisor);
-    Services.instance.database.clubs.update(club.toJson())
+    Services.instance.database.clubs.update(club.toJson());
   }
 }
