02c7b90bbf3da4c191e134089740895861a460ea
Levi Lesches <levilesches@gmail.com>
2021-07-18 16:00:00 -0400

    Schedule search (#84)
    * Added schedule_search.dart
    
    * Added schedule search model
    
    * Update lib/src/models/view/schedule_search.dart
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    * Update lib/src/models/view/schedule_search.dart
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    * Fixes some formatting
    
    * Added docs and exported schedule_search.dart in lib/models.dart
    
    * Cleaned up schedule_search.dart
    
    * Cleaned up models.dart
    
    * Fixed Ios issue
    
    * Update version number
    
    * Ios bug fixes
    
    * Fixes merge conflict
    
    * Add Period.dayName, and Period.name (#81)
    
    * Added Subject.id, PeriodData.name, PeriodData.dayName (#80)
    
    * Added Subject.id, PeriodData.name, PerioData.dayName
    
    * Update lib/src/data/schedule/subject.dart
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    * Allows querry to not be cap specific
    
    * Adds ability to search by subject id or name of the teacher.
    
    * Switched .toUppercase to .toLowercase.
    
    * Schedule search (#82)
    
    * added part of schedule_search UI
    
    * Hard coded buildResults
    
    * Finished schedule_search UI
    
    * Fixed minor formatting issues
    
    * Update lib/src/models/view/schedule_search.dart
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    * Update lib/src/models/view/schedule_search.dart
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>
    
    * Makes .toLowercase more effecient.
    
    * Fixes .toLowercase() issue
    
    * Update lib/src/models/view/schedule_search.dart
    
    * Update lib/src/models/view/schedule_search.dart
    
    * Schedule Search UI Enhancements (#88)
    
    * added part of schedule_search UI
    
    * Hard coded buildResults
    
    * Finished schedule_search UI
    
    * Fixed minor formatting issues
    
    * Enhanced the Schedule Search UI
    
    * Blue Zoom Links (#89)
    
    * added part of schedule_search UI
    
    * Hard coded buildResults
    
    * Finished schedule_search UI
    
    * Fixed minor formatting issues
    
    * Enhanced the Schedule Search UI
    
    * Made Zoom Links Blue
    
    * Removed Unused Imports
    
    Co-authored-by: todesj <josht2004@gmail.com>
    Co-authored-by: todesj <70974284+todesj@users.noreply.github.com>
    Co-authored-by: BraydenKO <58378705+BraydenKO@users.noreply.github.com>
    Co-authored-by: DavidTarrab <71798870+DavidTarrab@users.noreply.github.com>

--- lib/src/pages/schedule.dart
+++ lib/src/pages/schedule.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors_in_immutables
 import "package:flutter/material.dart";
 
+import "package:link_text/link_text.dart";
+
 import "package:ramaz/data.dart";
 import "package:ramaz/models.dart";
 import "package:ramaz/widgets.dart";
@@ -44,7 +46,19 @@ class ResponsiveSchedule extends NavigationItem<ScheduleViewModel> {
 	}
 
 	@override
-	AppBar get appBar => AppBar(title: const Text("Schedule"));
+	AppBar get appBar => AppBar(
+		title: const Text("Schedule"),
+		actions: [ Builder( 
+			builder: (BuildContext context) => IconButton(
+				icon: const Icon(Icons.search),
+				tooltip: "Search schedule",
+				onPressed: () => showSearch(
+					context: context,
+					delegate: CustomSearchDelegate(hintText: "Search for a class")
+				),
+			)
+		)]
+	);
 
 	@override
 	Widget? get floatingActionButton => Builder(
@@ -96,15 +110,202 @@ class ResponsiveSchedule extends NavigationItem<ScheduleViewModel> {
 					]
 				)
 			),
-			const SizedBox (height: 20),
-			const Divider(),
-			const SizedBox (height: 20),
+			const Divider(height: 40),
 			Expanded(
 				child: ClassList(
 					day: model.day, 
 					periods: Models.instance.user.data.getPeriods(model.day)
+				),
+			),
+		]
+	);
+}
+
+/// A class that creates the search bar using ScheduleModel.
+class CustomSearchDelegate extends SearchDelegate {
+	/// This model handles the searching logic.
+	final ScheduleSearchModel model = ScheduleSearchModel();
+
+	/// A constructor that constructs the search bar.
+	CustomSearchDelegate({
+		required String hintText,
+	}) : super(
+		searchFieldLabel: hintText,
+		keyboardType: TextInputType.text,
+		textInputAction: TextInputAction.search,
+	);
+
+	@override
+	Widget buildLeading(BuildContext context) => ElevatedButton(
+		onPressed: () => Navigator.of(context).pop(),
+		child: const Icon(Icons.arrow_back)
+	);
+
+	@override
+	Widget buildSuggestions(BuildContext context) {
+
+		final List<Subject> subjects = model.getMatchingClasses(query.toLowerCase());
+
+		return ListView(
+			children: [
+				const SizedBox(height: 15),
+				if (subjects.isNotEmpty)
+					for (Subject suggestion in subjects)
+						SuggestionWidget(
+							suggestion: suggestion,
+							onTap: () {
+								query = suggestion.name;
+								showResults(context);
+							},
+						)
+				else 
+					Column(
+						crossAxisAlignment: CrossAxisAlignment.center,
+						children: [ Text(
+							"No Results Found",
+							style: Theme.of(context).textTheme.headline4
+						)
+					])
+			]
+		);
+	}
+
+	@override
+	Widget buildResults(BuildContext context) { 
+
+		final List<Subject> subjects = model.getMatchingClasses(query.toLowerCase());
+
+		return ListView(
+			children: [
+				const SizedBox(height: 15),
+				if (subjects.isNotEmpty)
+					for (
+						PeriodData period in 
+						model.getPeriods(subjects.first)
+					)
+						ResultWidget(period)
+				else 
+					Column(
+						crossAxisAlignment: CrossAxisAlignment.center,
+						children: [ Text(
+							"No Results Found",
+							style: Theme.of(context).textTheme.headline4
+						)
+					])
+			]
+		);
+	}
+
+	@override
+	List<Widget> buildActions(BuildContext context) => [
+		if (query != "")
+			IconButton(
+				icon: const Icon(Icons.close),
+				onPressed: () => query = ""
+			)
+	];
+}
+
+/// A class that creates each individual suggestion.
+class SuggestionWidget extends StatelessWidget {
+
+	/// The function to be run when the suggestion is clicked.
+	final VoidCallback onTap;
+
+	/// The Subject given to the widget.
+	final Subject suggestion;
+
+	/// A constructor that defines what a suggestion should have.
+	const SuggestionWidget({
+		required this.suggestion,
+		required this.onTap
+	});
+
+	@override
+	Widget build(BuildContext context) => Column(
+		crossAxisAlignment: CrossAxisAlignment.start,
+		children: [ InkWell(
+			onTap: onTap,
+			child: Row(
+				children: [ Expanded(
+					child: Padding(
+						padding: const EdgeInsets.only(left: 20),
+						child: Column(
+							crossAxisAlignment: CrossAxisAlignment.start,
+							children: [ 
+								Text(
+									suggestion.name,
+									style: Theme.of(context).textTheme.headline4
+								),
+								const SizedBox(height: 5),
+								Text(
+									"${suggestion.teacher}   ${suggestion.id}",
+									style: Theme.of(context).textTheme.headline6
+								),
+								const SizedBox(height: 10),
+								if (suggestion.virtualLink != null)
+									LinkText(
+										"Link: ${suggestion.virtualLink}",
+										shouldTrimParams: true,
+										linkStyle: const TextStyle(color: Colors.blue)
+                	)
+							]
+						)
+					)
+				)
+			])
+		),
+		const Divider(
+			height: 20,
+			indent: 40,
+			endIndent: 40
+		)
+	]
+	);
+}
+
+/// A class that creates each individual result.
+class ResultWidget extends StatelessWidget {
+
+	/// The PeriodData given to the widget.
+	final PeriodData period;
+
+	/// A constructor that defines what a result should have.
+	ResultWidget(this.period);
+
+	@override
+	Widget build(BuildContext context) => Column(
+		crossAxisAlignment: CrossAxisAlignment.start,
+		children: [
+			ListTile(
+				title: Text(
+					period.dayName,
+						style: Theme.of(context).textTheme.headline4
+					),
+				subtitle: Text(
+					"Period ${period.name}   Room ${period.room}",
+					style: Theme.of(context).textTheme.headline6
 				)
 			),
+			for (int reminder in Models.instance.reminders.getReminders(
+				dayName: period.dayName,
+				period: period.name,
+				subject: Models.instance.user.subjects[period.id]?.name
+			))
+				Padding(
+					padding: const EdgeInsets.only(left: 7),
+					child: Row(
+						children: [
+							const Icon(Icons.notifications),
+							const SizedBox(width: 3),
+							Text(
+								Models.instance.reminders.reminders[reminder].message,
+								style: Theme.of(context).textTheme.subtitle1
+							)
+						]
+					)
+				),
+			const Divider(height: 20),
 		]
 	);
 }
