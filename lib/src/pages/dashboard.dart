db04138eb05c77d2fea5925680bee8205c3ce39b
David Tarrab <tarrabd@ramaz.org>
2021-12-22 23:09:10 -0500

    Made Tap For Schedule an Icon

--- lib/src/pages/dashboard.dart
+++ lib/src/pages/dashboard.dart
@@ -30,9 +30,7 @@ class Dashboard extends NavigationItem<DashboardModel> {
 				? Builder(
 						builder: (BuildContext context) => TextButton(
 							onPressed: () => Scaffold.of(context).openEndDrawer(),
-							child: const Text ("Tap for schedule",
-								style: TextStyle(color: Colors.blue),
-							),
+							child: const Icon(Icons.schedule),
 						)
 					)
 				: const SizedBox.shrink()
