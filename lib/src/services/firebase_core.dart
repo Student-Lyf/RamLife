6c61357a5bc6510a2dfa7e9144e7d638b9d39664
Levi Lesches <levilesches@gmail.com>
2022-01-17 23:54:10 -0500

    Fixes Firebase initialization on Web when using the emulator (#123)
    * Moved Firebase initialization logic to main using flutterfire_cli
    
    * Removed manual Firebase initialization for Web
    
    * Moved Dart-only Firebase initialization to FirebaseCore.initializeFirebase
    
    * Updated Firebase dependencies to support flutterfire_cli
    
    * Removed old (manual) calls to FirebaseCore.init(), replaced with new flutterfire_cli initialization
    
    * Added try/catch for Firestore emulator
      Workaround for https://github.com/FirebaseExtended/flutterfire/issues/6216
    
    * Cleanup

--- lib/src/services/firebase_core.dart
+++ lib/src/services/firebase_core.dart
@@ -1,5 +1,9 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:firebase_core/firebase_core.dart";
+import "package:flutter/foundation.dart";
+
+import "package:ramaz/firebase_options.dart";
 
 /// A wrapper around [Firebase].
 /// 
@@ -9,23 +13,27 @@ import "package:firebase_core/firebase_core.dart";
 /// of whether it has been initialized. 
 class FirebaseCore {
 	/// Whether the Firebase Local Emulator Suite should be used. 
-	///
-	/// This setting should be enabled during development ONLY. We can also use 
-	/// `kDebugMode` from `flutter/foundation`, but it's also valid to test UI 
-	/// or logic operations with the production database.
-	static bool shouldUseEmulator = false;
+	static bool shouldUseEmulator = kDebugMode;
 	
 	/// Whether Firebase has already been initialized.
 	static bool initialized = false;
 
-	/// Initializes Firebase if it hasn't already been. 
+	/// Initializes Firebase as configured by flutterfire_cli
 	static Future<void> init() async {
-		if (!initialized) {
-			await Firebase.initializeApp();
-			if(shouldUseEmulator){
-				await FirebaseAuth.instance.useAuthEmulator("localhost",9099);
+		await Firebase.initializeApp(
+		  options: DefaultFirebaseOptions.currentPlatform,
+		);
+		if (shouldUseEmulator) {
+			await FirebaseAuth.instance.useAuthEmulator("localhost", 9099);
+			// Setting the emulator after a hot restart breaks Firestore. 
+			// See: https://github.com/FirebaseExtended/flutterfire/issues/6216
+			try { FirebaseFirestore.instance.useFirestoreEmulator("localhost", 8080); }
+			catch (error) {  // throws a JavaScript object instead of a FirebaseException
+				final String code = (error as dynamic).code;
+				if (code != "failed-precondition") {
+					rethrow; 
+				}
 			}
-			initialized = true;
 		}
 	}
 }
