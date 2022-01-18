48906f0904a8e1baeb9358413b1d406afc1ddc74
Levi Lesches <levilesches@gmail.com>
2022-01-17 23:45:39 -0500

    Renamed SportsGame.link to .livestreamUrl

--- lib/src/widgets/atomic/sports_tile.dart
+++ lib/src/widgets/atomic/sports_tile.dart
@@ -243,8 +243,8 @@ class SportsTile extends StatelessWidget {
                 trailing: onTap == null ? IconButton(
                  icon: const Icon(Icons.live_tv),
                  onPressed: () {
-                   if(game.link!=null){
-                    launch(game.link!);
+                   if(game.livestreamUrl != null){
+                    launch(game.livestreamUrl!);
                    }},
                 ) : const Icon(Icons.edit),
               ),
